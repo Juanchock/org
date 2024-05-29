f7ccdda834ca9ca51953847289d58ef055ba0cbf
Juan Vanegas <jvanegas.vanegas@gmail.com>
2024-05-28 19:05:44 -0500

    commit

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,5 +1,5 @@
 import "./Equipo.css"
-
+import Colaborador from "../Colaborador"
 const Equipo = (props) =>{
 
     const {colorPrimario, colorSecundario, titulo} = props.datos
@@ -13,7 +13,12 @@ const estiloTitulo ={borderColor: colorPrimario}
 
     return <section className="equipo"style={obj} >
         <h3 style={estiloTitulo}>{titulo}</h3>
-        <div className="colaboradores"></div>
+        <div className="colaboradores">
+            <Colaborador />
+            <Colaborador />
+            <Colaborador />
+            <Colaborador />
+        </div>
 
     </section>
 }
