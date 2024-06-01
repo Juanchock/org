c0e906c230fc831df59416a9785d66175680ec9e
Juan Vanegas <jvanegas.vanegas@gmail.com>
2024-05-31 20:57:15 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import {v4 as uuid} from "uuid"
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
@@ -10,7 +11,88 @@ import Footer from './componentes/Footer';
 
 function App() {
   const[mostrarFormulario,actualizarMostrar] = useState(false)
-  const [colaboradores,actualizarColaboradores] =useState([])
+  const [colaboradores,actualizarColaboradores] =useState([{
+    id:uuid(),
+    equipo:"Front End",
+    foto:"https://github.com/JeanmarieAluraLatam.png",
+    nombre:"jUan",
+    puesto:"instructor"
+  },
+  {
+    id:uuid(),
+    equipo:"Programación",
+    foto:"https://github.com/JeanmarieAluraLatam.png",
+    nombre:"jkarol",
+    puesto:"gerente"
+  },
+  {
+    id:uuid(),
+    equipo:"UX y Diseño",
+    foto:"https://github.com/JeanmarieAluraLatam.png",
+    nombre:"sebas",
+    puesto:"contador"
+  },
+  {
+    id:uuid(),
+    equipo:"Programación",
+    foto:"https://github.com/JeanmarieAluraLatam.png",
+    nombre:"pepe",
+    puesto:"colaborador"
+  },
+
+
+
+
+
+])
+
+  const [equipos, actualizarEquipos] = useState ([
+    {
+      id: uuid(),
+      titulo: "Programación",
+      colorPrimario:"#57c278",
+      colorSecundario:"#d9f7e9"
+    },
+    {
+      id: uuid(),
+      titulo: "Front End",
+      colorPrimario:"#82cffa",
+      colorSecundario:"#e8f8ff"
+    },
+    {
+      id: uuid(),
+      titulo:  "Data Science",
+      colorPrimario:"#a6d157",
+      colorSecundario:"#f0f8e2"
+    },
+    {
+      id: uuid(),
+      titulo: "Devops",
+      colorPrimario:"#e06b69",
+      colorSecundario:"#fde7e8"
+    },
+    {
+      id: uuid(),
+      titulo: "UX y Diseño",
+      colorPrimario:"#db6ebf",
+      colorSecundario:"#fae9f5"
+    },
+    {
+      id: uuid(),
+      titulo: "Movil",
+      colorPrimario:"#ffba05",
+      colorSecundario:"#fff5d9"
+    },
+    {
+      id: uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario:"#ff8a29",
+      colorSecundario:"#ffeedf"
+    }
+ 
+])
+
+
 
   //Ternario--> condicion ? seMuestra  : noSeMuestra
   // condicion && seMuestra
@@ -26,48 +108,32 @@ const registrarColaborador = (colaborador) =>{
   //spread operator
   actualizarColaboradores([...colaboradores, colaborador])
 }
-  //Lista de equipos
 
-  const equipos = [
-    {
-      titulo: "Programación",
-      colorPrimario:"#57c278",
-      colorSecundario:"#d9f7e9"
-    },
-    {
-      titulo: "Front End",
-      colorPrimario:"#82cffa",
-      colorSecundario:"#e8f8ff"
-    },
-    {
-      titulo:  "Data Science",
-      colorPrimario:"#a6d157",
-      colorSecundario:"#f0f8e2"
-    },
-    {
-      titulo: "Devops",
-      colorPrimario:"#e06b69",
-      colorSecundario:"#fde7e8"
-    },
-    {
-      titulo: "UX y Diseño",
-      colorPrimario:"#db6ebf",
-      colorSecundario:"#fae9f5"
-    },
-    {
-      titulo: "Movil",
-      colorPrimario:"#ffba05",
-      colorSecundario:"#fff5d9"
-    },
-    {
-      titulo: "Innovación y Gestión",
-      colorPrimario:"#ff8a29",
-      colorSecundario:"#ffeedf"
-    }
+  //Actualizar color de equipo
+
+  const actulizarColor = (color, id)=> {
+    console.log("Actualizar:", color, id);
+    const equiposActualizados = equipos.map((equipo)=>{
+      if(equipo.id === id){
+        equipo.colorPrimario = color
+      }
+
+      return equipo
+    })
+
+    actualizarEquipos(equiposActualizados)
+  }
 
 
-    
-]
+  // Eliminar colaborador
+
+  const eliminarColaborador= () =>{
+    console.log("Eliminar colaborador");
+  }
+
+
+
+
 
   return (
     <div >
@@ -91,6 +157,8 @@ const registrarColaborador = (colaborador) =>{
     datos={equipo} 
     key={equipo.titulo} 
     colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo )}
+    eliminarColaborador={eliminarColaborador}
+    actulizarColor={actulizarColor}
       />
     )
    }
